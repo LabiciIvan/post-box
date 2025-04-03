07e8e3b7299b05c6dae4fb25789f697be1ed9c57
Labici Ivan <labici.ioan@yaoo.com>
2025-04-04 00:06:12 +0300

    PB-1: Modified App component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,47 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { logoutUser } from './hooks/Auth';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 import { AuthContext } from './context/AuthContext';
+import UnreadEmailCountContext from './context/ReadEmailContext';
+import { deleteLocalStorageForLoggedOutUser } from './tests/database-mock';
 
 const App = ({children} : {children: React.ReactNode}) => {
+
+  const unreadEmailsContext = useContext(UnreadEmailCountContext);
+
+  const {updateCount} = unreadEmailsContext;
 
   const auth = useContext(AuthContext);
 
   const { user } = auth;
 
-  if (!user) {
+  const location = useLocation();
+
+  const handleElegantUserLogout = (): void => {
+    if (!user) return;
+
+    const currentDate = new Date();
+
+    const userDate = new Date(user.logoutAfter);
+
+    if (userDate.getTime() < currentDate.getTime() && user.remeberLoggedIn === false) {
+      deleteLocalStorageForLoggedOutUser(user.id);
+      logoutUser(user);
+      updateCount(0);
+    }
+  }
+
+  // Adds an event listener to detect when the browser or tab is closed, 
+  // ensuring the user is automatically logged out if necessary.
+  document.addEventListener('visibilitychange', () => {
+    handleElegantUserLogout();
+    if (document.visibilityState === 'hidden' && user !== false) {
+      handleElegantUserLogout();
+    }
+  });
+
+  if (!user && location.pathname !== '/login') {
+    console.log('navigating outise', auth)
     return <Navigate to={'/login'}/>
   }
 
