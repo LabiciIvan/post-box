04280f8c7baeb264488ebf333767e387b6d8cd3a
Labici Ivan <ioanlabici@gmail.com>
2025-04-14 08:04:58 +0300

    PB-1: index.tsx and style changes.

--- src/index.tsx
+++ src/index.tsx
@@ -7,17 +7,24 @@ import '../node_modules/bootstrap-icons/font/bootstrap-icons.scss'
 import './index.scss';
 import ComposeProvider from './context/ComposeContext';
 import SearchProvider from './context/SearchContext';
+import LoginProvider from './context/LoginContext';
+import { UnreadEmailCountProvider } from './context/ReadEmailContext';
+import { FontContextProvider } from './context/FontContext';
 
 const rootElement = document.getElementById('root') as HTMLElement;
-
-
 
 createRoot(rootElement).render(
   <AuthProvider>
     <ViewSectionProvider>
       <ComposeProvider>
         <SearchProvider>
-          <Router />
+          <LoginProvider>
+            <UnreadEmailCountProvider>
+              <FontContextProvider>
+                <Router />
+              </FontContextProvider>
+            </UnreadEmailCountProvider>
+          </LoginProvider>
         </SearchProvider>
       </ComposeProvider>
     </ViewSectionProvider>
